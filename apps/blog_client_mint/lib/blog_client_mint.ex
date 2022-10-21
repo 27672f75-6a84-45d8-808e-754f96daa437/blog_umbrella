f93c7efcfe90b290061008eae0403bea25cd09d1
twozeronine <amdusias209@gmail.com>
2022-10-21 11:30:59 +0900

    feat : mint client 기본틀 구현

--- apps/blog_client_mint/lib/blog_client_mint.ex
+++ apps/blog_client_mint/lib/blog_client_mint.ex
@@ -1,2 +1,38 @@
 defmodule BlogClientMint do
+  def http_request(method, url, header, body) do
+    {:ok, conn} = connect()
+    {:ok, conn, request_ref} = Mint.HTTP.request(conn, method, url, header, body)
+
+    receive do
+      message ->
+        {:ok, conn, responses} = Mint.HTTP.stream(conn, message)
+
+        for response <- responses do
+          handle_response()
+        end
+
+        Mint.HTTP.close(conn)
+    end
+  end
+
+  defp connect() do
+    {:ok, conn} = Mint.HTTP.connect(:http, "localhost", 4000)
+  end
+
+  defp handle_response(response) do
+    case response do
+      {:status, ^request_ref, status_code} ->
+        IO.puts("Status: #{status_code}")
+
+      {:headers, ^request_ref, headers} ->
+        IO.puts("> Response headers: #{inspect(headers)}")
+
+      {:data, ^request_ref, data} ->
+        IO.puts("> Response body")
+        IO.puts(data)
+
+      {:done, ^request_ref} ->
+        IO.puts("> Response fully received")
+    end
+  end
 end
